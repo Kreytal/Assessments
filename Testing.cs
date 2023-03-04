8e21602107261908f770e4c35434f074cb2b4be2
Kreytal <anoslepums@gmail.com>
2023-03-04 22:25:46 +0000

    Testing 2

---
+++ Testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CMP1903M_A01_2223
+{
+	public class Testing
+	{
+		public Testing()
+		{
+			Pack pack = new Pack();
+		}
+	}
+}
